61f4486a4bdc0f97be0746d202ce96126a1f4f6a
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2023-11-08 16:32:47 +0100

    version secuencial de resImagen

---
+++ Practicas /Practica 5/resImagen.java
@@ -0,0 +1,46 @@
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class resImagen {
+    static int [][] iMatrix = new int[2560][1440];//Tomara valores aleatorios de 0 a 255
+    private static int m=2560,n=1440;
+    resImagen(){
+        
+        initializeMatrix();
+    }
+    public static void initializeMatrix(){
+        //Inicializamos los valores de la matriz
+        for(int i = 0; i < m; i++){
+            for(int j =0 ; j < n; j++){
+                iMatrix[i][j]= ThreadLocalRandom.current().nextInt(0, 255);
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        
+        //Inicializamos los valores de la matriz
+        
+
+        //Usamos la funcion dada para escalar 
+        
+        double inicTiempo = System.nanoTime();
+        
+        //Algoritmo
+        for(int i = 0; i < m; i++){
+            for(int j =0 ; j < n; j++){
+                try{
+                    iMatrix[i][j]= (int)(4*iMatrix[i][j]-iMatrix[i+1][j]-iMatrix[i][j+1]-iMatrix[i-1][j]-iMatrix[i][j-1])/8;
+                }catch(Exception e){
+                    //Si i o j estan out bound no me interesa calcularlo, por lo que capturo las excepciones.
+                }
+            }
+        }
+
+        double tiempoTotal = (System.nanoTime() - inicTiempo) / (double) 1.0e9;
+
+        System.out.println("El algoritmo ha tardado : "+ tiempoTotal + " segundos");
+
+    }
+
+}
